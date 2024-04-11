e8e2589f1802d18887744c54ab98b632ec203f9e
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-04-11 12:41:23 +1200

    Finshed off code

--- Main.py
+++ Main.py
@@ -5,10 +5,12 @@ connect = sqlite3.connect("Bball.db")
 c = connect.cursor()
 players = c.execute("SELECT * FROM Player;").fetchall()
 age = c.execute("SELECT Age FROM Player;").fetchall()
-name_age = c.execute("SELECT Firstname, Age FROM Player;").fetchall()
+name = c.execute("SELECT Firstname FROM Player;").fetchall()
 Lastname = c.execute("SELECT Lastname FROM Player;").fetchall()
+Team = c.execute("Select teamid FROM Player;").fetchall()
 
-user_input = input("Enter 1 to view all results. 2 For only age of players, enter 3 to see only Firstname and age of players, enter 4 to see last name of players: ")
+user_input = input("Hi please enter 1 to view all results in NBA players. 2 For only age of the players, enter 3 to see only Firstname of players, enter 4 to see last name of players, enter 5 to see Teams of Players: ")
+
 
 if user_input == "1":
     print(players)
@@ -17,7 +19,10 @@ if user_input == "2":
     print(age)
 
 if user_input == "3":
-    print(name_age)
+    print(name)
 
 if user_input == "4":
     print(Lastname)
+
+if user_input == "5":
+    print(Team)
