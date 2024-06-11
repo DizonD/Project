5dd43078f4f88ca0e609175fe099f1c723bc313a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-06-11 12:02:52 +1200

    commit

--- app.py
+++ app.py
@@ -14,11 +14,21 @@ def hello_world():
     connect.close()
     return render_template('home.html', players=players)
 
+@app.route("/allplayers")
+def allplayers():
+    connect = sqlite3.connect("Bball.db")
+    c = connect.cursor()
+    players = c.execute("""SELECT Player.id, Player.Firstname,
+                            Player.Lastname, Player.Imagefilename, Team.teamname FROM Player JOIN Team ON Player
+                            .Teamid=Team.team_id;""").fetchall()
+    connect.close()
+    return render_template('allplayers.html', players=players)
+
 @app.route('/player/<int:id>')
 def player(id):
     connect = sqlite3.connect('Bball.db')
     c = connect.cursor()
-    player = c.execute("""SELECT Player.id, Player.Firstname,Player.Lastname, Player.Imagefilename, Team.teamname FROM Player JOIN Team ON Player.Teamid=Team.team_id
+    player = c.execute("""SELECT * FROM Player JOIN Team ON Player.Teamid=Team.team_id
                         WHERE Player.id=?;""",(id,)).fetchone()
     connect.close()
     return render_template('player.html', player=player)
